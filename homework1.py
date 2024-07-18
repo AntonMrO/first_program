58574aee4b291b0a347f009df8116b6bbb1de7ca
AntonMrO <itoxio17@mail.ru>
2024-07-18 22:40:21 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,9 @@
+    #homework1 - String and index string
+
+example = "Paralellogram"
+
+print(example[0])           #first_symbol
+print(example[-1])          #end_symbol
+print(example[6:])          #second_half_word
+print(example[::-1])        #revers
+print(example[1::2])       #each_second_symbol
