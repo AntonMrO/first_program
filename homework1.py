ae926dbc978cb07d76f78c4633cd0f3eae206ca7
AntonMrO <itoxio17@mail.ru>
2024-07-18 22:45:41 +0300

    homework1.py

--- homework1.py
+++ homework1.py
@@ -3,7 +3,11 @@
 example = "Paralellogram"
 
 print(example[0])           #first_symbol
+
 print(example[-1])          #end_symbol
+
 print(example[6:])          #second_half_word
+
 print(example[::-1])        #revers
+
 print(example[1::2])       #each_second_symbol
