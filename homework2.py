99b237e6bad398e82272e3207c600fa635edfdb8
AntonMrO <itoxio17@mail.ru>
2024-07-18 23:40:55 +0300

    Add files via upload
    Практическое задание "Переменные"

---
+++ homework2.py
@@ -0,0 +1,7 @@
+ #homework 2 "Variables"
+
+number_of_completed_homeworks = 12
+number_of_hours_spent = 1.5
+name_of_course = 'Python'
+time_for_1_task = number_of_hours_spent / number_of_completed_homeworks
+print("Курс:",name_of_course,", всего задач:",number_of_completed_homeworks,", затрачено часов:",number_of_hours_spent,", среднее время выполнения одного задания:",time_for_1_task,"часа (",time_for_1_task * 60,"минут).")
