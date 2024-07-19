1ac7d33f262a29012bea15b67cb63bf6816d1e85
AntonMrO <itoxio17@mail.ru>
2024-07-20 01:00:13 +0300

    homework4
    Практический урок Организация программ и методы строк

---
+++ homework4.py
@@ -0,0 +1,11 @@
+    #homework4_orgfnization programs and methods of string
+
+my_string = input("Введите пословицу: ")
+print(len(my_string))
+
+# methods of string
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
