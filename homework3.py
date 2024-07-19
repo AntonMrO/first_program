7920c8e57757a786221b24d08343289979508fc8
AntonMrO <itoxio17@mail.ru>
2024-07-19 23:30:52 +0300

    Add files via upload
    Практическое задание: Динамическая типизация

---
+++ homework3.py
@@ -0,0 +1,9 @@
+    #homework3_dynamic_typization
+name = ("Anton")
+print("Name:",name)
+age = 36
+print("Age:",age)
+age = age + 3           #new value "age"
+print("New age:",age)
+is_student = True
+print("Is student:",is_student)
