66fb4b11bcd1fe079279afef6968f8e37fed468d
AntonMrO <itoxio17@mail.ru>
2024-08-04 10:31:47 +0300

    Add files via upload
    Домашняя работа по уроку Цикл for..

---
+++ module_2_4.py
@@ -0,0 +1,23 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes =[]
+
+for i in range(len(numbers)):
+    is_prime = True
+    if numbers[i] == 1:     #исключение 1, т.к. она не относится нги к простым ни к составным
+        continue
+
+    for j in range(2, numbers[i]):
+        if numbers[i] % j == 0 and numbers[i] > j:      #проверка остатка от деления на число, которое меньше проверяемого
+            is_prime = False
+            break
+        elif numbers[i] != j:
+            is_prime = True
+
+    if is_prime == False:                           #составление списка простых и не простых чисел в зависимости от флага is_Prime
+        not_primes.append(numbers[i])
+    elif is_prime == True:
+        primes.append(numbers[i])
+
+print('Простые числа:',primes)
+print('Не простые (составные):',not_primes)
